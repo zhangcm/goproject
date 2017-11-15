08081ca424afa91e32795ce654192b5644796737
zhangcm <zhangchaomin@qianmi.com>
2017-11-15 21:37:54 +0800

    fetch

---
+++ src/gopl.io/ch1/fetch/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strings"
+	"io/ioutil"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Status: %s\n", resp.Status)
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+
+}
---
+++ src/gopl.io/ch1/fetch1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"strings"
+	"io/ioutil"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Status: %s\n", resp.Status)
+		b, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+
+}
